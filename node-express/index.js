c2d476ced2056a0d8824dc9b51af4ece510aa9fd
Ritesh009 <riteshshots@gmail.com>
2019-05-08 15:47:41 +0530

    Express Serve Static File

--- node-express/index.js
+++ node-express/index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
+
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
+
+//Enables us to serve static pages 
+app.use(express.static(__dirname + '/public'));
 
 //next is used when you need to invoke a middleware
 app.use((req,  res, next) => {
-    console.log(req.header);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is Express Server!</h1></body></html>')
