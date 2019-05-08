b3d26db461e3495f99c833bf85c3e42baa856542
Ritesh009 <riteshshots@gmail.com>
2019-05-08 17:05:41 +0530

    Express Simple Rest

--- node-express/index.js
+++ node-express/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 
 const hostname = 'localhost';
@@ -8,11 +9,56 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res,  next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next(); //It will continue look for other functionalities for the endpoint '/dishes'
+});
+
+//Modified object will be passed on 
+app.get('/dishes', (req, res, next) =>{
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) =>{
+    res.end('Will add the dish: ' + req.body.name + '/n' +
+    ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) =>{
+    res.statusCode = 403; //403 operation not supported
+    res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) =>{
+    res.end('Deleting all the dishes!');
+});
+
+app.get('/dishes/:dishId', (req, res, next) =>{
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) =>{
+    res.statusCode = 403; //403 operation not supported
+    res.end('POST operation not supported on /dishes/ ' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) =>{
+    res.write('Updating the dish: ' + req.params.dishId);
+    res.end(' Will update the dish: ' + req.body.name + '\n' +
+    ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) =>{
+    res.end('Deleting all the dish: ' + req.params.dishId);
+});
 
 //Enables us to serve static pages 
 app.use(express.static(__dirname + '/public'));
 
-//next is used when you need to invoke a middleware
+//next() is used when you need to invoke a middleware
 app.use((req,  res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
