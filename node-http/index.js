c91b5c73724a989b7e993b3f1469bce5f9696313
Ritesh009 <riteshshots@gmail.com>
2019-05-08 15:11:59 +0530

    Node HTTP

---
+++ node-http/index.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = 'localhost';
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+    console.log("Request for " + req.url + " by method " + req.method);
+
+    if(req.method == 'GET'){
+        var fileUrl;
+        if(req.url == '/') fileUrl = '/index.html';
+        else  fileUrl = req.url;
+
+        var filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+        if(fileExt == '.html') {
+            //exist is callback function
+            fs.exists(filePath, (exists) => {
+                if(!exists){
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end('<html><body><h1>ERROR 404:' + fileUrl + ' Not Found! </h1></body></html>');
+
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<html><body><h1>ERROR 404:' + fileUrl + ' Not an HTML file! </h1></body></html>');
+
+            return;
+        }
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<html><body><h1>ERROR 404:' + req.method + ' Not Supported! </h1></body></html>');
+
+        return;  
+    }
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at ${hostname}:${port}`);
+});
