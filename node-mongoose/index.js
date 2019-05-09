56aa07112ac2effbd9a5378e8c781faa3048ebc7
Ritesh009 <riteshshots@gmail.com>
2019-05-09 15:39:05 +0530

    Mongoose SubDocuments

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -15,11 +15,26 @@ connect.then((db) => {
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id,{
+            $set: { description: 'Updated Test'}
+        },{
+            new: true   //return the updated dish
+        }).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
 
+        dish.comments.push({
+            rating: 5,
+            comment: 'Test Comment',
+            author: 'test author'
+        });
+
+        return dish.save();       
+    })
+    .then((dish) => {
+        console.log(dish);
+        
         return Dishes.deleteOne({});
     })
     .then(() => {
