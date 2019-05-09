90fd5bbd18f30ebcad0dfd4b19f90eae79c2dbda
Ritesh009 <riteshshots@gmail.com>
2019-05-09 15:19:44 +0530

    Mongoose - Create Method

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -8,21 +8,24 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected to the server!');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'test',
         description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log(dishes);
+
+        return Dishes.deleteOne({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.deleteOne({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 });
