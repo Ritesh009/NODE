464b6b8d634b5abb23d215f3c685180eb307b932
Ritesh009 <riteshshots@gmail.com>
2019-05-23 11:14:00 +0530

    Express Session Authentication

--- node-expressSession/app.js
+++ node-expressSession/app.js
@@ -43,38 +43,21 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 //Basic Authentication
 function auth(req, res, next) {
   console.log(req.session);
 
   if(!req.session.user){
-    var authHeader = req.headers.authorization;
-  
-    if(!authHeader) {
-      var err = new Error('You are not authenticated!');
+    var err = new Error('You are not authenticated!');
 
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401; //Unauthorized access
-      return next(err);
-    }
-
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-    var username = auth[0];
-    var password = auth[1];
-
-    if(username === 'admin' && password === 'password'){
-      req.session.user = 'admin';
-      next(); //Means the request will be forwared
-    } else {
-      var err = new Error('You are not authenticated!');
-
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401; //Unauthorized access
-      return next(err);
-    }
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401; //Unauthorized access
+    return next(err);
   } else {
-    if(req.session.user === 'admin'){
+    if (req.session.user === ''){
       next();
     } else {
       var err = new Error('You are not authenticated!');
@@ -89,8 +72,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
