04533477fb337f8e11bc475868d18ef4fd7c6d92
Ritesh009 <riteshshots@gmail.com>
2019-05-28 15:39:36 +0530

    Image  Upload using Multer

---
+++ node- fileUpload(Multer)/routes/uploadRouter.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const authenticate = require('../authenticate');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'public/images');
+  },
+
+  filename: (req, file, cb) => {
+      cb(null, file.originalname)
+  }
+});
+
+const imageFileFilter = (req, file, cb) => {
+    if(!file.originalname.match(/\.(jpg|jpeg|png|gig)$/)) {
+        return cb(new Error('You can upload only image files!'));
+    }
+    cb(null, true);
+}
+
+const upload = multer({ 
+    storage: storage, 
+    fileFilter: imageFileFilter
+});
+
+const uploadRouter = express.Router();
+
+uploadRouter.use(bodyParser.json());
+
+uploadRouter.route('/')
+.get(authenticate.verifyUser, (req, res, next) =>{
+    res.statusCode = 403; //403 operation not supported
+    res.end('GET operation not supported on /imageUpload');
+})
+.post(authenticate.verifyUser, 
+    upload.single('imageFile'),(req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(req.file);
+})
+.put(authenticate.verifyUser, (req, res, next) =>{
+    res.statusCode = 403; //403 operation not supported
+    res.end('PUT operation not supported on /dishes');
+})
+.delete(authenticate.verifyUser, (req, res, next) =>{
+    res.statusCode = 403; //403 operation not supported
+    res.end('DELETE operation not supported on /dishes');
+})
+
+module.exports = uploadRouter;
---
+++ node- fileUpload(Multer)/routes/users.js
@@ -0,0 +1,68 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+var passport = require('passport');
+var authenticate = require('../authenticate');
+
+var router = express.Router();
+router.use(bodyParser.json());
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signup', (req, res, next) => {
+  User.register(new User({username: req.body.username}), 
+  req.body.password, (err, user) =>
+  {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+      return;
+    }
+    else {
+      if(req.body.firstname)
+        user.firstname = req.body.firstname;
+      if (req.body.lastname)
+        user.lastname = req.body.lastname;
+      user.save((err, user) => {
+        if(err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+        }
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'Registration Successful!'});
+        });
+      });
+    }
+  });
+});
+
+router.post('/login', passport.authenticate('local'), (req, res, next) => {
+  
+  var token = authenticate.getToken({_id: req.user._id});
+  
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
+});
+
+router.get('/logout', (req, res, next) => {
+  if(req.session) {
+    req.session.destroy();  //Removes the session info from server side
+    res.clearCookie('session-id');
+    res.redirect('/'); //redirecting to homepage
+  } else {
+    var err = new Error('Your are not logged in!');
+    err.status = 403;
+    next(err);
+  }
+});
+
+module.exports = router;
